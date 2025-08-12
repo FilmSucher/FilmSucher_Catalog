e6bb934e4b1f001831ab52d1b2f6219de041ccc7
VaStan96 <vastan96@gmail.com>
2025-08-12 15:04:58 +0200

    fix DBConnections problem 4

--- src/main/java/film_sucher/catalog/entity/User.java
+++ src/main/java/film_sucher/catalog/entity/User.java
@@ -1,6 +1,8 @@
 package film_sucher.catalog.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ public class User {
     @Id
     private Long id;
     private String username;
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     public enum Role {
